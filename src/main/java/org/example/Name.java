9f8b467caab87cb18951b4f2ec5ceb89efbb29e2
Vincent <vincentverapen2012@gmail.com>
2021-06-04 16:29:36 -0400

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/example/Name.java
+++ src/main/java/org/example/Name.java
@@ -10,7 +10,7 @@ public class Name
         Scanner name = new Scanner(System.in);
         Greeter greeter = new Greeter();
         System.out.print("What is your name? ");
-        String newName = name.next();
+        String newName = name.nextLine();
         System.out.println("Hello " + newName + " nice to meet you!");
         String greetingString = greeter.greet(newName);
         System.out.println(greetingString);
